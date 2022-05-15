c20a2c77b43b1a8a22ef8afed43c655535fb55d1
pallobbiswas <pallobbiswas326@gmail.com>
2022-05-15 17:37:32 +0600

    title color change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = ({ children }) => {
       <input id='my-drawer-3' type='checkbox' class='drawer-toggle' />
       <div class='drawer-content flex flex-col'>
         <div class='w-full navbar bg-base-100 fixed top-0 z-50 lg:px-20'>
-          <div class='flex-1 px-2 mx-2 text-2xl'>Clean Co.</div>
+          <div class='flex-1 px-2 mx-2 text-3xl text-orange-500 font-bold'>Clean <span className="text-green-600 ml-2">Co.</span></div>
           <div class='flex-none lg:hidden'>
             <label for='my-drawer-3' class='btn btn-square btn-ghost'>
               <svg
