f7f6daa07261e12c9ce2d174383795198d6e4cba
pallobbiswas <pallobbiswas326@gmail.com>
2022-05-18 09:48:49 +0600

    part 2 done

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import fetecher from "../Api/axos.init.config";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    //iffe funciotn
+
+    (async () => {
+      const data = await fetecher.get("/service");
+      setServices(data.data);
+    })();
+  }, []);
+
+  console.log(services);
+
   return (
     <div>
       <h1>This is services</h1>
